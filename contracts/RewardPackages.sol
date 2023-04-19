78de538e65c797b6c22fce95b92b318f194f1305
tadeusz-cirocki <tadeusz.cirocki@widelab.co>
2023-04-19 15:26:22 +0200

    create package func

--- contracts/RewardPackages.sol
+++ contracts/RewardPackages.sol
@@ -3,17 +3,26 @@ pragma solidity ^0.8.9;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RewardPackages {
+contract RewardPackages is Ownable {
+    event PackageAdded(string name);
 
     // reward package struct
     struct Package {
         string name;
         bool isActive;
-        uint lockTime;  // lock time in seconds
+        uint lockTime; // lock time in seconds
         uint awardFrequency; // award freqency (interval) in seconds
         uint rewardPercentage; // 5 == 5%
         uint minDeposit;
         uint maxDeposit;
+    }
+
+    Package[] packages;
+
+    function createPackage(Package memory package) external onlyOwner {
+        packages.push(package);
+        emit PackageAdded(package.name);
     }
 }
